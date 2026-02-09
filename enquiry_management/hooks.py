1c1dc60bb01fca96ca671a0319038e81c0f10a0a
Vivdeveloper <50074551+Vivdeveloper@users.noreply.github.com>
2026-02-09 19:32:01 +0530

    chore: Update required_apps in hooks.py and remove unnecessary comment

--- enquiry_management/hooks.py
+++ enquiry_management/hooks.py
@@ -7,8 +7,7 @@ app_license = "mit"
 
 # Apps
 # ------------------
-# TEst By  vivek choudhary
-required_apps = ["frappe"]
+# tetst
 
 # Each item in the list will be shown as an app in the apps page
 # add_to_apps_screen = [
